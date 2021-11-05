a78c4bc529e78e14a17ce6072a9d472907a325ee
ugonfor <ugonfor@gmail.com>
2021-11-05 13:02:59 +0900

    Clean Directory

--- Week3/dlltest.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int func(int a, int b){
-    return a+b;
-}
--- Week3/exetest.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("%s", argv[0]);
-    return 0;
-}
