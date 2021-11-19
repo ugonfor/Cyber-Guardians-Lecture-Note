c4b70900b06e2fc5c226217e7fb38b29552a241b
ugonfor <ryuhyogon@gmail.com>
2021-11-20 00:53:47 +0900

    Update Week5/code

--- Week5/code/frida/hook.js
+++ Week5/code/frida/hook.js
@@ -11,17 +11,12 @@
      onEnter: function (args) {
          console.log('');
          console.log('[+] Called clickhandler(' + clickhandler + ')');
-         //console.log('[+] arg0: ' + args[0]);
          console.log('[+] x: ' + String(Number(args[0]) & 0xffff));
          console.log('[+] y: ' + String((Number(args[0]) & 0xffff0000) >> 16));
-         //dumpAddr('Input', args[1], args[3].toInt32());
-         //this.outptr = args[2]; // Store arg2 and arg3 in order to see when we leave the function
-         //this.outsize = args[3].toInt32();
      },
 
      // When function is finished
      onLeave: function (retval) {
-         //dumpAddr('Output', this.outptr, this.outsize); // Print out data array, which will contain de/encrypted data as output
          console.log('[+] Returned from clickhandler: ' + retval);
      }
  });
