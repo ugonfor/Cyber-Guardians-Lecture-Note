0c08265552c8bcb9c8fee1f52a8fdf8bc88c7d0f
ugonfor <ugonfor@gmail.com>
2021-11-26 17:09:14 +0900

    Add socket programming

---
+++ Week6/code/socket/client.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+#define BUF_SIZE 1024
+
+int main(int argc, char const *argv[])
+{
+    // client MUST take two command-line arguments
+    if (argc != 3) {
+        printf("usage: %s <IP address> <port>\n", argv[0]);
+        return 1;
+    }
+
+    // Argument1 : Server IP address 
+    const char* server_ip = argv[1];
+    // Argument2 : Server Port
+    const char* server_port = argv[2];
+
+    int socket_fd; // socket descriptor
+    struct sockaddr_in server_addr; // server socket address
+
+    // create socket 
+    // AF_INET : IPv4
+    // SOCK_STREAM : TCP
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1)
+    {
+        // if occur creation error
+        puts("socket creation error");
+        return 1;
+    }
+
+    // set the server address from arguments
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_addr.s_addr = inet_addr(server_ip); // server ip
+    server_addr.sin_family = AF_INET; // IPv4
+    server_addr.sin_port = htons(atoi(server_port)); // port number
+
+    // TCP connection with server
+    if(connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        // if occur connection error
+        puts("connect error\n");
+        close(socket_fd);
+        return 1;
+    }
+
+    while (true)
+    {
+        string s;
+        getline(cin, s);
+        s += "\r\n";
+        
+        // sends the message to the server
+        ssize_t res = send(socket_fd, s.c_str(), s.size(), 0);
+		if (res == 0 || res == -1) {
+			cerr << "send return " << res;
+			perror(" ");
+			break;
+		}
+    }
+        
+    // close socket descriptor
+    close(socket_fd);
+
+    return 0;
+}
